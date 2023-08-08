120967b220e93b204c880a8de1256ade5dcbc9ff
naks1 <naks1@LAPTOP-ALF4K35C>
2023-08-07 20:32:51 -0500

    Add project files.

---
+++ Elephant.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapElephant
+{
+    class Elephant
+    {
+		private string name;
+        private int earSize;
+        public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}		
+		public int EarSize
+		{
+			get { return earSize; }
+			set { earSize = value; }
+		}
+
+		public void WhoAmI()
+		{
+            Console.WriteLine("My name is {0}", Name);
+            Console.WriteLine("My ears are {0} inches long", EarSize);
+        }
+
+		public void HearMessage(string message, Elephant whoSaidIt)
+		{
+            Console.WriteLine(Name + " heard a message");
+            Console.WriteLine(whoSaidIt.Name + " said this: " + message);
+        }
+
+		public void SpeakTo(Elephant whoToTalkTo, string message)
+		{
+			whoToTalkTo.HearMessage(message, this) ;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace SwapElephant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+            Elephant lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
+
+            Console.Write("Press 1 for Lloyd, 2 for Lucinda, 3 to swap ");
+
+            #region MyWay
+            //while (true)
+            //{
+            //    string line = Console.ReadLine();
+            //    if (int.TryParse(line, out int i) )
+            //    {
+            //        if (i == 1) lloyd.WhoAmI(); 
+            //        if (i == 2) lucinda.WhoAmI();
+            //        if (i == 3)
+            //        {
+            //            Elephant newElephant = lucinda;
+            //            lucinda = lloyd;
+            //            lloyd = newElephant;
+            //            Console.WriteLine("References have been swapped");
+
+            //        }                                  
+            //    }
+                #endregion
+
+            while (true)
+            {
+                char input = Console.ReadKey(true).KeyChar;
+                Console.WriteLine();
+                Console.WriteLine("You pressed " + input);
+
+
+                if (input == '1')
+                {
+                    Console.WriteLine("Calling lloyd.WhoAmI()");
+                    lloyd.WhoAmI();
+                }
+                else if (input == '2')
+                {
+                    Console.WriteLine("Calling lucinda.WhoAmI()");
+                    lucinda.WhoAmI();
+                }
+                else if (input == '3')
+                {
+                    Elephant holder;
+                    holder = lloyd;
+                    lloyd = lucinda;
+                    lucinda = holder;
+                    Console.WriteLine("References have been swapped");
+                }
+                else if (input == '4')
+                {
+                    lloyd = lucinda;
+                    lloyd.EarSize = 4321;
+                    lloyd.WhoAmI();
+                }
+                else if (input == '5')
+                {
+                    lucinda.SpeakTo(lloyd, "Hi Lloyd!");
+                }
+
+                else return;
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
